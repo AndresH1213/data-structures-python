a46651fd08d3332f44518d01c639b7308fcab483
Andres Hernandez <edanhebla1213@hotmail.com>
2022-04-13 23:34:01 -0500

    implementing algorithm

--- pathfinding/astar.py
+++ pathfinding/astar.py
@@ -101,6 +101,26 @@ def h(p1, p2):
     return abs(x1 - x2) + abs(y1 - y2)
 
 
+def algorithm(draw, grid, start, end):
+    count = 0
+    open_set = PriorityQueue()
+    open_set.put((0, count, start))
+    came_from = {}
+    g_score = {spot: float('inf') for row in grid for spot in row}
+    g_score[start] = 0
+    f_score = {spot: float('inf') for row in grid for spot in row}
+    f_score[start] = h(start.get_pos(), end.get_pos())
+
+    open_set_hash = {start}
+
+    while not open_set.empty():
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+
+        current = open_set.get()[2]
+
+
 def make_grid(rows, width):
     grid = []
     gap = width // rows
@@ -187,7 +207,12 @@ def main(win, width):
 
             if event.type == pygame.KEYDOWN:
                 if event.key == pygame.K_SPACE and not started:
-                    pass
+                    for row in grid:
+                        for spot in row:
+                            spot.update_neighbors()
+
+                    algorith(lambda: draw(win, grid, ROWS, width),
+                             grid, start, end)
 
     pygame.quit()
 
